e94830046793be3538e714857b621125a6696e39
carlschh <carlschh@iu.edu>
2021-09-22 09:04:19 -0400

    css

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -5,26 +5,38 @@ const app = {
       }
     },
 
-    created() {
-      fetch('https://randomuser.me/api/')
-      .then(response => response.json()) //response is parameter =>
-      .then((parsedJson) => {
-        console.log(parsedJson); //checks data - debugger lite
-        this.person = parsedJson.results[0]
-        this.dob = this.person.dob.date[5] + this.person.dob.date[6] + this.person.dob.date[7] + this.person.dob.date[8] + this.person.dob.date[9] + this.person.dob.date[4] +this.person.dob.date[0] + this.person.dob.date[1] +this.person.dob.date[2] +this.person.dob.date[3]
-      })
-      .catch(err => {
-        console.error(err)
-      })
-
-    },
+    // computed: {
+    //   prettyBirthday() {
+    //     return this.person.dob.date;
+    //   }
+    // },
 
     methods: {
       refreshPage(){
         window.location.reload();
+      },
+      
+      fetchUserData() {
+        fetch('https://randomuser.me/api/')
+        .then(response => response.json()) //response is parameter =>
+        .then((parsedJson) => {
+          console.log(parsedJson); //checks data - debugger lite
+          this.person = parsedJson.results[0]
+          this.dob = this.person.dob.date[5] + this.person.dob.date[6] + this.person.dob.date[7] + this.person.dob.date[8] + this.person.dob.date[9] + this.person.dob.date[4] +this.person.dob.date[0] + this.person.dob.date[1] +this.person.dob.date[2] +this.person.dob.date[3]
+        })
+        .catch(err => {
+          console.error(err)
+        })
+
       }
 
-    }
+    },
+
+    // created() {
+    //   this.fetchUserData();
+    // }
+
+    
   }
   
 Vue.createApp(app).mount('#results');
@@ -47,3 +59,5 @@ Vue.createApp(app).mount('#results');
 //   .catch(errFunc) 
 
 // }
+
+// Resources : https://stackoverflow.com/questions/62975890/how-to-reload-refresh-a-webpage-in-vue-js-after-a-certain-event-or-a-button-is-c
