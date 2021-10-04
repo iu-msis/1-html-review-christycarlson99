e761a7f73f10edb0557b8cf4bbe8661cce09cb19
carlschh <carlschh@iu.edu>
2021-10-04 13:46:41 -0400

    in class 10-4

---
+++ app/public/api/student/index.php
@@ -0,0 +1,33 @@
+<?php
+// require 'common.php';
+require 'class/dBConnection.php';
+
+echo ":::";
+// print_R($_ENV);
+echo getenv('MYSQL_DATABASE');
+
+
+// Step 1: Get a datase connection from our helper class
+$db = DbConnection::getConnection();
+
+// Step 2: Create & run the query
+$sql = 'SELECT * FROM student';
+$vars = [];
+
+// if (isset($_GET['guid'])) {
+//   // This is an example of a parameterized query
+//   $sql = 'SELECT * FROM Patient WHERE patientGuid = ?';
+//   $vars = [ $_GET['guid'] ];
+// }
+
+$stmt = $db->prepare($sql);
+$stmt->execute($vars);
+
+$students = $stmt->fetchAll();
+
+// Step 3: Convert to JSON
+$json = json_encode($students, JSON_PRETTY_PRINT);
+
+// Step 4: Output
+header('Content-Type: application/json');
+echo $json;
